81853e4346d60cc0f984ba24e96afe37f1e49c84
Matt <matthew.adams@optimaconnect.co.uk>
2024-08-07 17:03:49 +0100

    First version of app that deploys trained model to predict

--- new_game_prediction_app.R
+++ new_game_prediction_app.R
@@ -5,13 +5,43 @@ library(ggplot2)
 model <- readRDS("model.rds")
 df <- readRDS("data.rds")
 
+# define function to replace an input with Other if it has no match in the data
+other_replace <- function(df, col_name, input) {
+  
+  if (input == "") {
+    "Other"
+  } else if (max(str_detect(df[[col_name]], input)) == 1){
+    input
+  } else {
+    "Other"
+  }
+}
+
+cols <- df[c("Reviewscore", 
+             "Publisher", 
+             "Franchise", 
+             "Launch_Year",
+             "Play_Year",
+             "Category",
+             "DLC_Played",
+             "Perspective",
+             "Played_On")]
+
 ui <- fluidPage(
   titlePanel("Predicting my Game Ratings!"),
   sidebarLayout(
     sidebarPanel(
-      titlePanel("Input New Game"),
+      h3("Input New Game"),
       textInput("ng", "Game Name:", value = "New Game"),
       sliderInput("rs", "Reviewscore:", min = 0, max = 100, value = 75),
+      textInput("pub", "Publisher:"),
+      textInput("fran", "Franchise:"),
+      numericInput("ly", "Launch Year:", value = format(Sys.Date(), "%Y")),
+      numericInput("py", "Play Year:", value = format(Sys.Date(), "%Y")),
+      selectInput("cat", "Main Category:", choices = unique(df$Category)),
+      selectInput("persp", "Perspective:", choices = unique(df$Perspective)),
+      selectInput("po", "Played On:", choices = unique(df$Played_On)),
+      selectInput("dlc", "DLC:", choices = unique(df$DLC_Played))
       
     )
     ,
@@ -24,10 +54,28 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  prediction <- reactive(input$rs)
+  prediction <- reactive({
+    
+    new_game <- as_tibble(cbind(input$rs, 
+                                other_replace(df, "Publisher", input$pub),
+                                other_replace(df, "Franchise", input$fran),
+                                input$ly,
+                                input$py,
+                                input$cat,
+                                input$persp,
+                                input$po,
+                                input$dlc))
+    
+    names(new_game) <- names(cols)
+    
+    model_input <- rbind(cols, new_game)
+    
+    predict(model, tail(model_input, 1))
+    
+  })
   
   output$plot <- renderPlot({
-    plot(df$Reviewscore, df$Rating, pch = 20, col = 'grey', alpha = 0.7)
+    plot(df$Reviewscore, df$Rating, pch = 20, col = 'grey')
     points(input$rs, prediction(), col = 'red', pch = 4, cex = 1.5)
   }, res = 96)
   
