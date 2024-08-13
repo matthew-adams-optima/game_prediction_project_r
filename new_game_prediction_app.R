fe2d31b241f53bbc677399a98e3f0ee2d05436e3
Matt <matthew.adams@optimaconnect.co.uk>
2024-08-13 11:46:57 +0100

    Made hover tooltip go near mouse pointer

--- new_game_prediction_app.R
+++ new_game_prediction_app.R
@@ -47,8 +47,12 @@ ui <- fluidPage(
     ,
     mainPanel(
       textOutput("prediction"),
-      plotOutput("plot", hover = hoverOpts(id = "plot_hover")),
-      verbatimTextOutput("hover_info"),
+      div(
+        style = "position:relative",
+        plotOutput("plot", 
+                   hover = hoverOpts(id = "plot_hover", delay = 100, delayType = "debounce")),
+        uiOutput("hover_info")
+      ),
       plotOutput("band", height = "100px")
     )
   )
@@ -92,18 +96,35 @@ server <- function(input, output) {
       
   }, res = 96)
   
-  output$hover_info <- renderPrint({
+  output$hover_info <- renderUI({
+    hover <- input$plot_hover
+    point <- nearPoints(df, hover, threshold = 5, maxpoints = 1, addDist = TRUE)
+    if (nrow(point) == 0) return(NULL)
     
-    if(!is.null(input$plot_hover)){
-      hover=input$plot_hover
-      dist=sqrt((hover$x-df$Reviewscore)^2+(hover$y-df$Rating)^2)
-      cat("Game:")
-      if(min(dist) < 3)
-        df$Game[which.min(dist)]
-    }
+    # calculate point position INSIDE the image as percent of total dimensions
+    # from left (horizontal) and from top (vertical)
+    left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+    top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
     
+    # calculate distance from left and bottom side of the picture in pixels
+    left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
+    top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
     
+    # create style property fot tooltip
+    # background color is set so tooltip is a bit transparent
+    # z-index is set so we are sure are tooltip will be on top
+    style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
+                    "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+    
+    # actual tooltip created as wellPanel
+    wellPanel(
+      style = style,
+      p(HTML(paste0("<b> Game: </b>", point$Game, "<br/>",
+                    "<b> Rating: </b>", point$Rating, "<br/>",
+                    "<b> Review Score: </b>", point$Reviewscore, "<br/>")))
+    )
   })
+  
   
   output$prediction <- renderText({
     paste("Expected Rating for ", input$ng, " is ", prediction())
