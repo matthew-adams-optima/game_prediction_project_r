0500ea247b79e99a5aa86021b249c015d01e8d2b
Matt <matthew.adams@optimaconnect.co.uk>
2024-09-02 17:01:49 +0100

    Adding to gbm optimisation and trying lasso

--- model_data_prep.R
+++ model_data_prep.R
@@ -80,9 +80,13 @@ group_factor(df, "Category", 3)
 #test and train split
 train_ind <- as_vector(createDataPartition(df$Rating, p = 0.75))
 
-
 train <- df[train_ind, ] #select all rows with the training indexes
 test <- df[-train_ind, ] #select all rows not with the training indexes
+
+## First benchmark for just predicting reviewscore ##
+
+cor(test$Rating, test$Reviewscore)^2 #0.4781 R-squared
+sqrt(mean((test$Rating - test$Reviewscore)^2)) #10.789 mean square error
 
 ## first model, linear using basic reviewscore only, for a benchmark ##
 
@@ -92,8 +96,12 @@ summary(model1) # Adjusted R-squared 0.5498 on training data
 prediction <- predict(model1, test)
 predict_df1 <- add_column(test, prediction)
 cor(predict_df1$Rating, predict_df1$prediction)^2 # 0.4781 R-squared in test data
+sqrt(mean((predict_df1$Rating - predict_df1$prediction)^2)) #8.922 mean square error
 
-# base model formula
+# R-squared is the same as just using review score as expected, but error is reduced
+# Due to systematic error/ misalignment of means. So this basic model outperforms just using reviewscore mostly due to intercept term
+
+# base model formula to use from now
 fmla <- formula(Rating ~ Reviewscore
 + Publisher 
 + Franchise
@@ -165,32 +173,88 @@ test_data <- test[c("Rating",
                       "Perspective",
                       "Played_On")]
 
-x_matrices <- glmnet::makeX(train = train_data[, !names(train_data) == "Rating"],
+x_matrices <- makeX(train = train_data[, !names(train_data) == "Rating"],
                             test = test_data[, !names(test_data) == "Rating"])
 
-###Calculate best lambda 
+##Train model
 cv.out <- cv.glmnet(x_matrices$x, train_data$Rating,
                     alpha = 0, nlambda = 100,
-                    lambda.min.ratio = 0.0001)
+                    lambda.min.ratio = 0.0001,
+                    nfolds = 5)
 
-best_lambda = cv.out$lambda.min
-
-model4 <- glmnet(x_matrices$x, train_data$Rating, alpha = 0, lamda = best_lambda)
-
+y_predicted <- predict(cv.out, s = cv.out$lambda.min, newx = x_matrices$x)
 # training R-squared
-y_predicted <- predict(model4, s = best_lambda, newx = x_matrices$x)
-
 tss <- sum((train$Rating - mean(train$Rating))^2)
 rss <- sum((train$Rating - y_predicted)^2)
 1 - rss/tss # 0.7373 training R-squared
 
 #test R-squared
-y_predicted <- predict(model4, s = best_lambda, newx = x_matrices$xtest)
+y_predicted <- predict(cv.out, s = cv.out$lambda.min, newx = x_matrices$xtest)
 
 tss <- sum((test$Rating - mean(test$Rating))^2)
 rss <- sum((test$Rating - y_predicted)^2)
-1 - rss/tss # 0.4725 test R-squared
-# strong overfitting here
+1 - rss/tss # 0.4740 test R-squared
+sqrt(mean((test$Rating - y_predicted)^2)) # 8.884 mean square error
+
+## Lasso regression ##
+
+#Same as ridge above but change alpha from 0 to 1
+
+cv.out <- cv.glmnet(x_matrices$x, train_data$Rating,
+                    alpha = 1, nlambda = 100,
+                    lambda.min.ratio = 0.0001,
+                    nfolds = 5)
+
+# training R-squared
+y_predicted <- predict(cv.out, s = cv.out$lambda.min, newx = x_matrices$x)
+
+tss <- sum((train$Rating - mean(train$Rating))^2)
+rss <- sum((train$Rating - y_predicted)^2)
+1 - rss/tss # 0.6855 training R-squared
+
+#test R-squared
+y_predicted <- predict(cv.out, s = cv.out$lambda.min, newx = x_matrices$xtest)
+
+tss <- sum((test$Rating - mean(test$Rating))^2)
+rss <- sum((test$Rating - y_predicted)^2)
+1 - rss/tss # 0.5125 test R-squared
+sqrt(mean((test$Rating - y_predicted)^2)) # 8.552 mean square error
+
+# Lasso better than ridge
+
+## Alpha can be set to any number, so is there a sweet spot in the middle? ##
+set.seed(2)
+alphas <- 100
+plot(NULL, xlim = c(0, 1), ylim = c(0.45, 0.55), xlab = "depth", ylab = "test rsq")
+for (i in 0:alphas) {
+  cv.out <- cv.glmnet(x_matrices$x, train_data$Rating,
+                      alpha = i/alphas, nlambda = 100,
+                      lambda.min.ratio = 0.0001,
+                      nfolds = 5)
+  
+  best_lambda = cv.out$lambda.min
+  
+  y_predicted <- predict(cv.out, s = best_lambda, newx = x_matrices$xtest)
+  
+  tss <- sum((test$Rating - mean(test$Rating))^2)
+  rss <- sum((test$Rating - y_predicted)^2)
+  rsq <- 1 - rss/tss
+  print(paste(i/alphas, rsq))
+  points(i/alphas, rsq)
+}
+# 0 = ridge regression is the worst but then pretty equal from 0.2 or higher. Go with 0.5
+
+# convert to using a glmnet regular model once settled #
+
+cv.out <- cv.glmnet(x_matrices$x, train_data$Rating,
+                    alpha = 0.5, nlambda = 100,
+                    lambda.min.ratio = 0.0001)
+
+best_lambda = cv.out$lambda.min
+
+model <- glmnet(x_matrices$x, train_data$Rating, alpha = 0.5, lamda = best_lambda)
+
+plot(model, xvar = "lambda", label = TRUE)
 
 ## Gradient boosted regressor ##
 
@@ -213,11 +277,11 @@ predict_test <- add_column(test, prediction)
 cor(predict_test$Rating, predict_test$prediction)^2 # 0.4922 R-squared in test data
 
 residuals <- predict_test$Rating - predict_test$prediction
-sqrt(mean(residuals^2)) # rss
+sqrt(mean(residuals^2)) # mean square error
 
 gbm.perf(model5)
 
-# gbm probably best, let's reduce overfitting and maximise test performance #
+# Let's reduce overfitting and maximise test performance for the gbm model #
 
 ## optimise number of trees ##
 gbm.perf(model5, method = "cv") # about 60 is best
@@ -227,7 +291,7 @@ gbm.perf(model5, method = "cv") # about 60 is best
 set.seed(2)
 min_depth <- 1
 max_depth <- 3
-plot(NULL, xlim = c(min_depth, max_depth), ylim = c(0, 1), xlab = "depth", ylab = "test rss")
+plot(NULL, xlim = c(min_depth, max_depth), ylim = c(0, 1), xlab = "depth", ylab = "test rsq")
 for (i in min_depth:max_depth) {
   model = gbm(fmla
                , data = train
@@ -245,15 +309,61 @@ for (i in min_depth:max_depth) {
 
 ## 1 is best!
 
-## Improve model formula by considering interaction terms ##
+## optimise learning rate ##
+
+set.seed(2)
+min_learn <- 1
+max_learn <- 100
+plot(NULL, xlim = c(min_learn/1000, max_learn/1000), ylim = c(0, 1), xlab = "depth", ylab = "test rsq")
+for (i in min_learn:max_learn) {
+  model = gbm(fmla
+              , data = train
+              , distribution = "gaussian"
+              , n.trees = 60
+              , shrinkage = i/1000
+  )
+  prediction <- predict(model, test)
+  predict_test <- add_column(test, prediction)
+  rsq <- cor(predict_test$Rating, predict_test$prediction)^2
+  print(paste(i/1000, rsq))
+  points(i/1000, rsq)
+  
+}
+
+# About 0.07 seems optimal with 100 trees or 0.08 with 60 trees
+
+## Adjust minobsinnode ##
+
+set.seed(2)
+min_obs <- 1
+max_obs <- 100
+plot(NULL, xlim = c(min_obs, max_obs), ylim = c(0, 1), xlab = "depth", ylab = "test rsq")
+for (i in min_obs:max_obs) {
+  model = gbm(fmla
+              , data = train
+              , distribution = "gaussian"
+              , n.trees = 60
+              , shrinkage = 0.08
+              , n.minobsinnode = i
+  )
+  prediction <- predict(model, test)
+  predict_test <- add_column(test, prediction)
+  rsq <- cor(predict_test$Rating, predict_test$prediction)^2
+  print(paste(i, rsq))
+  points(i, rsq)
+  
+}
+
+# steady downnward slope, okay with low number for regression so leave blank!
 
 ## Try model again and get scores with the improvements ##
 
 set.seed(2)
 model = gbm(fmla
-             , data = train
-             , distribution = "gaussian"
-             , n.trees = 60
+            , data = train
+            , distribution = "gaussian"
+            , n.trees = 60
+            , shrinkage = 0.08
 )
 
 summary(model) # relative importances
@@ -267,7 +377,11 @@ predict_test <- add_column(test, prediction)
 cor(predict_test$Rating, predict_test$prediction)^2 # 0.4922 R-squared in test data
 
 residuals <- predict_test$Rating - predict_test$prediction
-sqrt(mean(residuals^2)) # rss
+sqrt(mean(residuals^2)) # mean square error
+
+## Lasso model still best so let's try optimising that ##
+
+
 
 ## Re-train using full sample for best model ##
 
@@ -275,6 +389,7 @@ final_model = gbm(fmla
                   , data = df
                   , distribution = "gaussian"
                   , n.trees = 60
+                  , shrinkage = 0.08
 )
 
 prediction <- predict(final_model, df)
